bf56360103caaa94ffc13bf1b075ef01083c6c44
Suresh Paudel <sureshpaudel59@gmail.com>
2025-08-15 16:00:23 +0545

    Updated

--- components/filter/SearchAndSort.tsx
+++ components/filter/SearchAndSort.tsx
@@ -18,7 +18,7 @@ const SearchAndSort: React.FC<SearchAndSortProps> = ({
       {/* Search */}
       <input
         type="text"
-        placeholder="Search tasks..."m
+        placeholder="Search tasks..."
         value={searchKeyword}
         onChange={(e) => onSearchChange(e.target.value)}
         className="border border-gray-300 rounded-md px-3 py-2 text-sm w-full sm:w-60 focus:outline-none focus:ring-2 focus:ring-blue-500"
